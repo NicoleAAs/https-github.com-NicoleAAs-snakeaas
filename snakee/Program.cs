3c770cbd0df59015ed1cd6e71489b85729b79248
Nikol Nicole <aasakuyla@gmail.com>
2020-08-25 20:42:58 +0300

    Добавьте файлы проекта.

---
+++ snakee/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace snakee
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			point p1 = new point(1, 3, '*');
+			p1.Draw();
+
+			point p2 = new point(4, 5, '#');
+			p2.Draw();
+
+			Console.ReadLine();
+		}
+
+	}
+}
